33be6fbb1c472251a02e73d130ce78f81a85f52e
talha270 <144779558+talha270@users.noreply.github.com>
2025-01-26 17:14:58 +0500

    added

---
+++ android/app/src/main/kotlin/com/example/foodprimeapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodprimeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,55 @@
+
+
+import 'package:flutter/material.dart';
+
+import '../../theme/style.dart';
+import 'nav_pages/cart/cart_page.dart';
+import 'nav_pages/home/home_page.dart';
+import 'nav_pages/profile/profile_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  int _currentPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        currentIndex: _currentPageIndex,
+        selectedItemColor: primaryColorED6E1B,
+        selectedLabelStyle: const TextStyle(color: primaryColorED6E1B),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded), label: "Account"),
+        ],
+      ),
+      body: _switchPagesOnIndex(_currentPageIndex)
+    );
+  }
+
+  _switchPagesOnIndex(int index) {
+    switch(index) {
+      case 0: {
+        return const HomePage();
+      }
+      case 1 : {
+        return const CartPage();
+      }
+      case 2: {
+        return const ProfilePage();
+      }
+    }
+  }
+}
